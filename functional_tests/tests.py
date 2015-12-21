94075f28acaa8ade9b53ce8edf80f38165bef542
q <q@q.q>
2015-12-21 12:01:02 +0100

    Prepare FT to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,22 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
+import sys
 class NewVisitorTest(StaticLiveServerTestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url='http://'+arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url=cls.live_server_url
+
+	@classmethod
+	def tearUpClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()	
 
 	def setUp(self):
 		self.browser=webdriver.Firefox()
@@ -19,7 +33,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
 		#User goes to app's homepage
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 		#User notices that page title includes words "to-do"
 		self.assertIn('To-Do',self.browser.title)
@@ -56,7 +70,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 #User 2 visits homepage and there is no sign of User 1 content
 
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Become a programmer guru',page_text)
 		self.assertNotIn('philosophy', page_text)
@@ -81,7 +95,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_layout_and_styling(self):
 		#User goes to the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024,768)
 
 		#Input box is centered
